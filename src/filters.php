82d23ce6757259382d6ce95a4002a1111c33e4f6
Tom Broucke <tom@tombroucke.be>
2020-12-11 21:50:23 +0100

    check if add_filter is available

--- src/filters.php
+++ src/filters.php
@@ -1,10 +1,16 @@
 <?php //phpcs:ignore
-namespace Otomaties\ACF_Objects;
 
-\add_filter('acf/format_value', function( $value, $post_id, $field ){
-    $class = '\\Otomaties\\ACF_Objects\\' . ucfirst( $field['type'] );
-	if ( class_exists( $class ) ) {
-		$value = new $class( $value, $post_id, $field );
-    }
-    return $value;
-});
+if ( ! function_exists( 'add_filter' ) ) {
+	return;
+}
+
+add_filter(
+	'acf/format_value',
+	function( $value, $post_id, $field ) {
+		$class = '\\Otomaties\\ACF_Objects\\' . ucfirst( $field['type'] );
+		if ( class_exists( $class ) ) {
+			$value = new $class( $value, $post_id, $field );
+		}
+		return $value;
+	}
+);
