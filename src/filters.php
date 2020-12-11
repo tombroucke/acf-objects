6396c4cfcc1a82308cfc8b405c4879d5db17c856
Tom Broucke <tom@tombroucke.be>
2020-12-11 21:46:54 +0100

    filter namespace

--- src/filters.php
+++ src/filters.php
@@ -1,7 +1,7 @@
 <?php //phpcs:ignore
 namespace Otomaties\ACF_Objects;
 
-add_filter('acf/format_value', function( $value, $post_id, $field ){
+\add_filter('acf/format_value', function( $value, $post_id, $field ){
     $class = '\\Otomaties\\ACF_Objects\\' . ucfirst( $field['type'] );
 	if ( class_exists( $class ) ) {
 		$value = new $class( $value, $post_id, $field );
