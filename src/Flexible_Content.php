24dbe41598c35bcdadc1dfa550a2d96cc0364b08
Tom Broucke <tom@tombroucke.be>
2021-01-04 12:11:09 +0100

    Added support for flexible content

---
+++ src/Flexible_Content.php
@@ -0,0 +1,27 @@
+<?php //phpcs:ignore
+namespace Otomaties\AcfObjects;
+
+use Otomaties\AcfObjects\Abstracts\ListField;
+use Otomaties\AcfObjects\Flexible_Content\Row;
+
+class Flexible_Content extends ListField {
+
+	public function value() {
+		if ( is_array( $this->value ) ) {
+			return $this->value;
+		}
+		return array();
+	}
+
+	public function offsetGet( $offset ) {
+		if ( isset( $this->value[ $offset ] ) ) {
+			return new Row( $this->value[ $offset ] );
+		}
+		return null;
+	}
+
+	public function current() {
+		$value = current( $this->value );
+		return new Row( $value );
+	}
+}
---
+++ src/Flexible_Content/Row.php
@@ -0,0 +1,20 @@
+<?php //phpcs:ignore
+namespace Otomaties\AcfObjects\Flexible_Content;
+
+class Row {
+
+	protected $row;
+
+	public function __construct( array $row = array() ) {
+		$this->row = $row;
+	}
+
+	public function get( $key ) {
+		return $this->row[ $key ];
+    }
+
+    public function layout() {
+        return $this->get('acf_fc_layout');
+    }
+
+}
