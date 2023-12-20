b8ab01a18872a00b3116727dfe4b8093746ad2fd
Tom Broucke <tom@tombroucke.be>
2023-12-20 14:23:18 +0100

    added URL

---
+++ src/Url.php
@@ -0,0 +1,13 @@
+<?php
+namespace Otomaties\AcfObjects;
+
+use Otomaties\AcfObjects\Abstracts\Field;
+use Otomaties\AcfObjects\Traits\Stringable;
+
+/**
+ * Url Field
+ */
+class Url extends Field
+{
+    use Stringable;
+}
