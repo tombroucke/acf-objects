8bc2400e2f10646f1c3d6d55b1dc8ece52194444
tombroucke <tom@tombroucke.be>
2024-10-08 17:52:45 +0200

    Update Select.php

--- src/Fields/Select.php
+++ src/Fields/Select.php
@@ -2,9 +2,11 @@
 
 namespace Otomaties\AcfObjects\Fields;
 
+use Otomaties\AcfObjects\Traits\Numeric;
 use Otomaties\AcfObjects\Traits\Stringable;
 
 class Select extends Field
 {
     use Stringable;
+    use Numeric;
 }
