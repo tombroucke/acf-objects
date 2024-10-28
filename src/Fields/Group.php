159f6c38c53b7f148c825429f928aa37a1471613
Tom Broucke <tom@tombroucke.be>
2024-10-28 11:49:30 +0100

    🩹 fix: Set group default

--- src/Fields/Group.php
+++ src/Fields/Group.php
@@ -2,11 +2,27 @@
 
 namespace Otomaties\AcfObjects\Fields;
 
+use Otomaties\AcfObjects\Contracts\CollectionContract;
+
 class Group extends Collection
 {
+    public function default(mixed $values): CollectionContract
+    {
+        if ($this->isEmpty()) {
+            $this->items = $values;
+        } else {
+            foreach ($values as $key => $value) {
+                if (! isset($this->items[$key])) {
+                    $this->items[$key] = $value;
+                }
+            }
+        }
+
+        return $this;
+    }
+
     public static function prepareData(mixed $data): mixed
     {
-
         if (is_bool($data) && ! $data) {
             $data = [];
         }
