5b0a42b0bd66b18bd2d778c9e250271b7d9b6f55
Tom Broucke <tom@tombroucke.be>
2024-12-11 09:57:46 +0100

    📚 docs: Added docblock

--- src/Facades/AcfObjects.php
+++ src/Facades/AcfObjects.php
@@ -3,7 +3,14 @@
 namespace Otomaties\AcfObjects\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Otomaties\AcfObjects\Contracts\CollectionContract;
+use Otomaties\AcfObjects\Contracts\FieldContract;
 
+/**
+ * @method static FieldContract | CollectionContract | bool getField(string $selector, mixed $postId = false, bool $formatValue = true)
+ *
+ * @see \Otomaties\AcfObjects\AcfObjects
+ */
 class AcfObjects extends Facade
 {
     /**
